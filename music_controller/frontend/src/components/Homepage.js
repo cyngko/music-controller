6c313a313c73f38a28c9c21c7ac1e42542ccc23f
Christian Boenigk <christianboenigk@gmx.de>
2020-11-23 15:35:18 +0100

    update

--- music_controller/frontend/src/components/Homepage.js
+++ music_controller/frontend/src/components/Homepage.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
+import Room from './Room';
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,16 +10,23 @@ import {
   Redirect,
 } from 'react-router-dom';
 
-export default function Homepage() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path='/'>
-          <h1>This is the Homepage</h1>
-        </Route>
-        <Route path='/join' component={RoomJoinPage} />
-        <Route path='/create' component={CreateRoomPage} />
-      </Switch>
-    </Router>
-  );
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <p>This is the home page</p>
+          </Route>
+          <Route path='/join' component={RoomJoinPage} />
+          <Route path='/create' component={CreateRoomPage} />
+          <Route path='/room/:roomCode' component={Room} />
+        </Switch>
+      </Router>
+    );
+  }
 }
